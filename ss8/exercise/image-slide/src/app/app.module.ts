1aaa96db09976ef33b6cf6ef998cf9bc779740f6
unknown <hoangdinh37k02@gmail.com>
2022-06-16 21:07:03 +0700

    ss8

--- ss8/exercise/image-slide/src/app/app.module.ts
+++ ss8/exercise/image-slide/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
-
-import { AuthService } from './auth.service';
 import {ImgSliderModule} from './img-slider/img-slider/img-slider.module';
-
 
 @NgModule({
   declarations: [
@@ -14,22 +12,20 @@ import {ImgSliderModule} from './img-slider/img-slider/img-slider.module';
   imports: [
     BrowserModule,
     ImgSliderModule,
+    // ImageGalleryModule,
+    // ContentProjectionModule
   ],
   // providers: [AuthService],
   providers: [
-    {provide: AuthService, useClass: AuthService},
+    // {provide: AuthService, useClass: AuthService},
     {provide: 'API_ENDPOINT', useValue: 'http://api.example.com'},
     {provide: 'API_URL', useExisting: 'API_ENDPOINT'},
     {
       provide: 'some-token',
-      // tslint:disable-next-line:only-arrow-functions
       useFactory() {
         return Math.random();
       }
     },
-    // {provide: 'API_URL', useValue: 'abc.com'},
-    // {provide: 'API_URL', useExisting: 'api.com'}
-    {provide: GalleryConfig, useValue: 2}
   ],
   bootstrap: [AppComponent]
 })
--- ss8/exercise/slide/src/app/app.component.ts
+++ ss8/exercise/slide/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercise-ss8';
-  constructor() {}
+  title = 'slide';
 }
